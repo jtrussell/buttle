8ac4f2723acb229587b624fae7a7c097f43e5df7
Justin <jus.russell@gmail.com>
2014-02-14 08:54:45 -0500

    feature: Add live reload capability
    For html and md files

--- index.js
+++ index.js
@@ -3,17 +3,31 @@
 module.exports = function(opts) {
   'use strict';
 
+  var port = opts.port || 8080
+    , webroot = process.cwd();
+
   var connect = require('connect')
     , http = require('http');
 
+  var indexes = [
+    /index\.html/i,
+    /readme\.md/i,
+    /readme\.markdown/i
+  ];
+
   var app = connect()
+    .use(require('./lib/mid-normalize')(indexes))
     .use(require('./lib/mid-logger')())
-    .use(require('./lib/mid-buttle')(process.cwd()))
-    .use(connect.static(process.cwd()))
+    .use(require('./lib/mid-lr')(opts.watch))
+    .use(require('./lib/mid-buttle')(webroot))
+    .use(connect.static(webroot))
     .use(function(req, res){
       res.end('A thousand apologies, but there are none to be had.\n');
     });
 
-  console.log('Listening on port ' + opts.port);
-  http.createServer(app).listen(opts.port);
+  console.log('Listening on port ' + port);
+  http.createServer(app).listen(port);
+
+  require('./lib/live-reload-server')(opts.watch);
 };
+
